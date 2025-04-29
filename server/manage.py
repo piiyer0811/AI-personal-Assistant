6537b80b36ae434a1501bb6142e6f279dc3be31d
Pratik Iyer <pratikiyer00@gmail.com>
2025-04-29 18:13:53 +0200

    dummy commit

--- server/manage.py
+++ server/manage.py
@@ -12,8 +12,9 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
+            "please check on the path"
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment?"  
         ) from exc
     execute_from_command_line(sys.argv)
 
